8f0ae5fb9ff1e104b31205ab5beb3c5f3bbbd0ed
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-04-20 14:49:24 -0400

    working through exercise and finished three functions.  understanding more

--- power.js
+++ power.js
@@ -1,7 +1,15 @@
 // document.querySelector("#activate-flight").addEventListener()
 // Add the correct string as the first argument to addEventListener()
-document.querySelector("#activate-flight").addEventListener("click", )
+
+document.querySelector("#activate-flight").addEventListener("click", function(){
+    const flightHandlerFunction = document.querySelector("#flight");
+    flightHandlerFunction.classList.toggle("enabled")
+    console.log("you clicked the activate flight button")
+    flightHandlerFunction.classList.toggle("disabled").remove
+} )
+
 // Write a function named flightHandlerFunction that will remove the disabled class on the corresponding <section id="flight"> (section, not button) and replace it with a class of enabled.
+
 // Have your developer tools open. When you click the button, the following element...
 // <section id="flight" class="power disabled">
 // Should then look like this.
@@ -9,8 +17,20 @@ document.querySelector("#activate-flight").addEventListener("click", )
 // <section id="flight" class="power enabled">
 // Once you have that working, write two more JavaScript functions, and two more querySelector().addEventListener() statements for handling the click event on the other two buttons. Those event handlers should perform the same step of removing disabled, and adding enabled on the corresponding <section> elements
 
-// document.querySelector("#activate-mindreading").addEventListener()
-// document.querySelector("#activate-xray").addEventListener()
+document.querySelector("#activate-mindreading").addEventListener("click", function(){
+    const mindReaderFunction = document.querySelector("#mindreading");
+    mindReaderFunction.classList.toggle("enabled")
+    console.log("you pressed the mind reading button")
+    mindReaderFunction.classList.toggle("disabled").remove
+})
+
+document.querySelector("#activate-xray").addEventListener("click", function(){
+    const xrayFunction = document.querySelector("#xray");
+    xrayFunction.classList.toggle("enabled")
+    console.log("Thanks for clicking activate xray")
+    xrayFunction.classList.toggle("disabled").remove
+})
+
 // Once that is complete, add two more buttons
 
 // Enable All Powers
