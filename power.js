88834a0796f969f2b99032d2e570229b0a402955
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-04-20 15:57:28 -0400

    review

--- power.js
+++ power.js
@@ -2,11 +2,25 @@
 // Add the correct string as the first argument to addEventListener()
 
 document.querySelector("#activate-flight").addEventListener("click", function(){
+    console.log("you clicked the activate flight button")
     const flightHandlerFunction = document.querySelector("#flight");
     flightHandlerFunction.classList.toggle("enabled")
-    console.log("you clicked the activate flight button")
     flightHandlerFunction.classList.toggle("disabled").remove
 } )
+
+//Jordan's solution
+// function flightHandlerFunction(){
+//     console.log("you clicked the activate flight button")
+// }
+
+// document.querySelector("#activate-flight").addEventListener("click", flightHandlerFunction)
+
+// document.querySelector("activate-flight").addEventListener("click", function(){
+// const flightSection = document.querySelector("#flight")
+// console.log(flightSection)
+// flightSection.classList.remove("disabled")
+// flightSection.classList.add("enabled")
+// })
 
 // Write a function named flightHandlerFunction that will remove the disabled class on the corresponding <section id="flight"> (section, not button) and replace it with a class of enabled.
 
@@ -33,7 +47,7 @@ document.querySelector("#activate-xray").addEventListener("click", function(){
 
 // Once that is complete, add two more buttons
 document.querySelector("#activate-all").addEventListener("click", function(){
-    const activateAllFunction = document.querySelectorAll("#powerList");
+    const activateAllFunction = document.querySelectorAll(".power");
     
     activateAllFunction.classList.add("enabled")
     console.log("activate-all clicked")
@@ -43,3 +57,19 @@ document.querySelector("#activate-all").addEventListener("click", function(){
 // Enable All Powers
 // Disable All Powers
 // Write two more event handlers for activating and deactivating all powers when the corresponding buttons are clicked. You will need to use the document.querySelectorAll() method for these.
+
+//jordan's solution
+
+function activateAllPowers(){
+    activateMindReading();
+    activateFlight();
+    activateXrayVision();
+}
+
+document.querySelector("#activate-flight").addEventListener("click", activateFlight)
+document.querySelector("#activate-mindreading").addEventListener("click", activateMindReading)
+document.querySelector("#activate-xray").addEventListener("click", activateXrayVision)
+
+document.querySelector("#activate-all").addEventListener("click", activateAllPowers)
+
+//select the activate all 
